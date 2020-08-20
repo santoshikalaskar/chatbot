2938227de867a06ccd9c639e3a68932d1723d4e4
santoshi <kalaskars1996@gmail.com>
2020-08-21 00:07:14 +0530

    rasa call API using python

---
+++ rasa_api_call_using_python.py
@@ -0,0 +1,16 @@
+import requests
+url = 'http://localhost:5005/webhooks/rest/webhook'
+payload = {"sender":"mee","message": "what is codincub?"}
+
+import json
+r = requests.post(url, data=json.dumps(payload))
+
+# Response, status etc
+print(r.text)
+print(r.status_code)
+
+data = r.json()
+print(data[0].get("text"))
+
+# using curl command in terminal
+#curl -XPOST -d '{"message":"hello","sender":"me"}' 'http://localhost:5005/webhooks/rest/webhook'
