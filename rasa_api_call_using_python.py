ef2dae7d7e00ee77d6fbf5fee93c67922f0ace12
santoshi <kalaskars1996@gmail.com>
2020-08-23 13:08:06 +0530

    update code for calling rasa api using python

--- rasa_api_call_using_python.py
+++ rasa_api_call_using_python.py
@@ -1,19 +1,46 @@
-import requests
-url = 'http://localhost:5005/webhooks/rest/webhook'
-payload = {"sender":"mee","message": "what is codincub?"}
+# import requests
+# url = 'http://localhost:5005/webhooks/rest/webhook'
+# payload = {"sender":"mee","message": "what is codincub?"}
 
-import json
-r = requests.post(url, data=json.dumps(payload))
+# import json
+# r = requests.post(url, data=json.dumps(payload))
 
 # Response, status etc
-print(r.text)
-print(r.status_code)
+# print(r.text)
+# print(r.status_code)
 
-data = r.json()
-print(data[0].get("text"))
+# data = r.json()
+# print(data[0].get("text"))
 
 # using curl command in terminal
 # curl -XPOST -d '{"message":"hello","sender":"me"}' 'http://localhost:5005/webhooks/rest/webhook'
 
 # run rasa project 
 # rasa run --enable-api --debug --cors "*"
+
+import requests
+import json
+import pandas as pd
+
+url = 'http://localhost:5005/webhooks/rest/webhook'
+# reading csv file
+dataframe = pd.read_csv("Chatbot_Daily_Report.csv")
+print("<---- Dataframe Columns ---->")
+print(dataframe.columns)
+
+questions = dataframe['question']
+Response_list = []
+Question_list= []
+for ques in questions:
+    payload = {"sender":"mee","message": ques}
+    r = requests.post(url, data=json.dumps(payload))
+    response_return = r.json()
+    Question_list.append(ques)
+    Response_list.append(response_return[0].get("text"))
+    #print("\n")
+
+d = {'Questions':Question_list,'Rasa_response':Response_list}
+Rasa_dataframe = pd.DataFrame(d)
+
+Rasa_dataframe.to_csv('Rasa_output_dataframe.csv')
+
